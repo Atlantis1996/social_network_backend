bc191741396efd7c467949be42f449d57d108a23
zhaoxuanli <zhx_li1996@berkeley.edu>
2020-10-17 05:34:55 -0400

    task1

--- src/main/java/edu/cmu/cc/minisite/ProfileServlet.java
+++ src/main/java/edu/cmu/cc/minisite/ProfileServlet.java
@@ -151,7 +151,7 @@ public class ProfileServlet extends HttpServlet {
             pst.setString(2, pwd);
             System.out.println(pst.toString());
             ResultSet resultSet = pst.executeQuery();
-            result.next();
+            resultSet.next();
             profile_image_url = resultSet.getString("profile_photo_url");
             System.out.println(profile_image_url);
         } catch (Exception e) {
