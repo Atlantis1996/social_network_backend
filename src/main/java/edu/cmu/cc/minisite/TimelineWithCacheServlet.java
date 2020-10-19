04fe62e89e8a438f2921932ba74580af08a9db88
zhaoxuanli <zhx_li1996@berkeley.edu>
2020-10-18 22:13:47 -0400

    task

--- src/main/java/edu/cmu/cc/minisite/TimelineWithCacheServlet.java
+++ src/main/java/edu/cmu/cc/minisite/TimelineWithCacheServlet.java
@@ -49,7 +49,84 @@ public class TimelineWithCacheServlet extends HttpServlet {
     /**
      * Your initialization code goes here.
      */
+
+    /**
+     * The Neo4j driver.
+     */
+    private final Driver driver;
+
+    /**
+     * The endpoint of the database.
+     *
+     * To avoid hardcoding credentials, use environment variables to include
+     * the credentials.
+     *
+     * e.g., before running "mvn clean package exec:java" to start the server
+     * run the following commands to set the environment variables.
+     * export NEO4J_HOST=...
+     * export NEO4J_NAME=...
+     * export NEO4J_PWD=...
+     */
+    private static final String NEO4J_HOST = System.getenv("NEO4J_HOST");
+    /**
+     * Neo4J username.
+     */
+    private static final String NEO4J_NAME = System.getenv("NEO4J_NAME");
+    /**
+     * Neo4J Password.
+     */
+    private static final String NEO4J_PWD = System.getenv("NEO4J_PWD");
+    
+    /**
+     * The endpoint of the database.
+     *
+     * To avoid hardcoding credentials, use environment variables to include
+     * the credentials.
+     *
+     * e.g., before running "mvn clean package exec:java" to start the server
+     * run the following commands to set the environment variables.
+     * export MONGO_HOST=...
+     */
+    private static final String MONGO_HOST = System.getenv("MONGO_HOST");
+    /**
+     * MongoDB server URL.
+     */
+    private static final String URL = "mongodb://" + MONGO_HOST + ":27017";
+    /**
+     * Database name.
+     */
+    private static final String DB_NAME = "reddit_db";
+    /**
+     * Collection name.
+     */
+    private static final String COLLECTION_NAME = "posts";
+    /**
+     * MongoDB connection.
+     */
+    private static MongoCollection<Document> collection;
+    /**
+     * Your initialization code goes here.
+     */
     public TimelineWithCacheServlet() {
+        Objects.requireNonNull(NEO4J_HOST);
+        Objects.requireNonNull(NEO4J_NAME);
+        Objects.requireNonNull(NEO4J_PWD);
+        driver = getDriver();
+
+        Objects.requireNonNull(MONGO_HOST);
+        MongoClientURI connectionString = new MongoClientURI(URL);
+        MongoClient mongoClient = new MongoClient(connectionString);
+        MongoDatabase database = mongoClient.getDatabase(DB_NAME);
+        collection = database.getCollection(COLLECTION_NAME);
+    }
+
+    private Driver getDriver() {
+        Objects.requireNonNull(NEO4J_HOST);
+        Objects.requireNonNull(NEO4J_NAME);
+        Objects.requireNonNull(NEO4J_PWD);
+        return GraphDatabase.driver(
+                "bolt://" + NEO4J_HOST + ":7687",
+                AuthTokens.basic(NEO4J_NAME, NEO4J_PWD));
     }
 
     /**
@@ -86,12 +163,162 @@ public class TimelineWithCacheServlet extends HttpServlet {
      * @param id user id
      * @return timeline of this user
      */
+
     private String getTimeline(String id) {
         // TODO: implement this method
         JsonObject result = new JsonObject();
         // Use the given cache variable to implement your
         // caching mechanism.
+        String rs = cache.get(id)
+        if(rs != null) {
+            return rs;
+        }
+        JsonArray followers = getFollowers(id);
+        JsonArray followees = getFollowees(id);
+        String profile_image_url = getUrl(id);
+        JsonArray comments = get30Comments(followees);
+
+        result.add("followers", followers);
+        result.add("comments", comments);
+        result.addProperty("profile", profile_image_url);
+        result.addProperty("name", id);
+        if(followers.size() > 300) {
+            cache.put(id, result.toString());
+        }
         return result.toString();
+    }
+
+    public JsonArray get30Comments(JsonArray followees) {
+        JsonArray comments = new JsonArray();
+        JsonObject parentComment, grandParentComment;
+        Bson filter = Filters.eq("uid", "dummy"); 
+
+        String name, parentId, grandParentId;            
+        JsonObject followee;
+        for(JsonElement fl : followees) {
+            followee = fl.getAsJsonObject();
+            name = followee.get("name").getAsString();
+            System.out.println(name);
+            filter = Filters.or(filter, Filters.eq("uid", name));
+        }
+
+
+        
+        MongoCursor<Document> cursor = collection
+                        .find(filter)
+                        .sort(Sorts.descending("ups", "timestamp"))
+                        .projection(Projections.fields(Projections.excludeId()))
+                        .limit(30)
+                        .iterator();
+        int i=0;
+        try {
+            while (cursor.hasNext()) {
+                i++;
+                JsonObject comment = new JsonParser().parse(cursor.next().toJson()).getAsJsonObject();
+              //TODO   
+                parentId = comment.get("parent_id").getAsString();
+                parentComment = null;
+                parentComment = getParentComment(parentId);
+                if(parentComment != null) {
+                    comment.add("parent", parentComment);
+                    grandParentId = parentComment.get("parent_id").getAsString();
+                    grandParentComment = null;
+                    grandParentComment = getParentComment(grandParentId);
+                    if(grandParentComment != null) {
+                        comment.add("grand_parent", grandParentComment);
+                    }
+                }
+                comments.add(comment);
+                System.out.println(i);
+             }    
+         } catch (Exception e){
+            e.printStackTrace();
+         }finally {
+             cursor.close();
+         }
+
+        return comments;
+    }
+
+    public JsonObject getParentComment(String parentId) {
+        JsonObject parentComment = null;
+        MongoCursor<Document> parentCursor = collection
+                                    .find(Filters.eq("cid", parentId))
+                                    .projection(Projections.fields(Projections.excludeId()))
+                                    .iterator();
+        try {
+            while (parentCursor.hasNext()) {
+                    parentComment = new JsonParser().parse(parentCursor.next().toJson()).getAsJsonObject();
+                }
+        } finally {
+            parentCursor.close();
+        }
+        return parentComment;
+    }
+
+    public String getUrl(String id) {
+        String profile_image_url;
+        Map<String,Object> parameters = Collections.singletonMap( "username", id );
+        Record record;
+        try (Session session = driver.session()) {
+            StatementResult rs = session.run("MATCH (follower:User)-[r:FOLLOWS]->(followee:User) WHERE followee.username = $username RETURN followee", parameters);
+            record = rs.next();
+            profile_image_url = record.get(0).get("url").asString();
+        }
+        return profile_image_url;
+    }
+
+
+    public JsonArray getFollowers(String id) {
+        JsonArray followers = new JsonArray();
+        JsonObject follower = new JsonObject();
+        String follower_name, profile_image_url;
+        Record record;
+        Map<String,Object> parameters = Collections.singletonMap( "username", id );
+
+        try (Session session = driver.session()) {
+            StatementResult rs = session.run("MATCH (follower:User)-[r:FOLLOWS]->(followee:User) WHERE followee.username = $username RETURN follower ORDER BY follower.username", parameters);
+            while (rs.hasNext()) {
+                record = rs.next();
+                follower = new JsonObject();
+
+                follower_name = record.get(0).get("username").asString();
+                profile_image_url = record.get(0).get("url").asString();
+
+                follower.addProperty("profile", profile_image_url);
+                follower.addProperty("name", follower_name);
+                followers.add(follower);
+
+            }
+        }
+    
+        return followers;
+    }
+
+    public JsonArray getFollowees(String id) {
+        JsonArray followees = new JsonArray();
+        JsonObject followee = new JsonObject();
+        String followee_name, profile_image_url;
+        Record record;
+        Map<String,Object> parameters = Collections.singletonMap( "username", id );
+
+        try (Session session = driver.session()) {
+            StatementResult rs = session.run("MATCH (follower:User)-[r:FOLLOWS]->(followee:User) WHERE follower.username = $username RETURN followee ORDER BY follower.username", parameters);
+            while (rs.hasNext()) {
+                record = rs.next();
+                followee = new JsonObject();
+
+                followee_name = record.get(0).get("username").asString();
+                profile_image_url = record.get(0).get("url").asString();
+
+                followee.addProperty("profile", profile_image_url);
+                followee.addProperty("name", followee_name);
+                followees.add(followee);
+
+            }
+        }
+    
+        return followees;
     }
 }
 
